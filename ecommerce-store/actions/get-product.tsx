b921185caf12a9f80c27e31966af390b892994c2
Gourav Rajak <106438974+gouravrajak985@users.noreply.github.com>
2025-05-26 21:59:08 +0530

    Delete ecommerce-store directory

--- ecommerce-store/actions/get-product.tsx
+++
@@ -1,9 +0,0 @@
-import { Product } from '@/types';
-
-const getProduct = async (id: string, storeUrl?: string): Promise<Product> => {
-  const URL = `${storeUrl || process.env.NEXT_PUBLIC_API_URL}/products`;
-  const res = await fetch(`${URL}/${id}`);
-  return res.json();
-};
-
-export default getProduct;
